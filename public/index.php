e193595ad96007c9659d7e05b376045e377cc725
Mohammed Fathy <mohammed1fathy1@gmail.com>
2025-05-19 13:52:54 +0300

    fixing cors error

--- public/index.php
+++ public/index.php
@@ -54,17 +54,19 @@ function cors()
     $allowedOrigins = [
         'https://scandiweb-test.free.nf',
         'http://localhost:3000', // For local development
+        'http://www.hameed.vip', // Production domain
     ];
 
     $origin = $_SERVER['HTTP_ORIGIN'] ?? '';
 
     if (in_array($origin, $allowedOrigins)) {
         header("Access-Control-Allow-Origin: $origin");
+        header("Access-Control-Allow-Credentials: true");
+        header("Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS");
+        header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
     }
 
     if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-        header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
-        header('Access-Control-Allow-Headers: Content-Type, Authorization');
         header('Access-Control-Max-Age: 86400'); // 24 hours cache
         exit(0);
     }
